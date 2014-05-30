50ad0aa3266a3c45b308cac8015186d3b0e47c70
hrach <hrach.cz@gmail.com>
2014-05-30 21:28:43 +0200

    Added post detail

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -13,10 +13,27 @@ class HomepagePresenter extends BasePresenter
 	/** @var \OrmDemo\Orm @inject */
 	public $orm;
 
+	/** @var Post */
+	private $post;
+
 
 	public function renderDefault()
 	{
 		$this->template->posts = $this->orm->posts->findHomepageOverview();
+	}
+
+
+	public function actionDetail($id)
+	{
+		$post = $this->orm->posts->getById($id);
+		if (!$post) $this->error();
+		$this->post = $post;
+	}
+
+
+	public function renderDetail()
+	{
+		$this->template->post = $this->post;
 	}
 
 }
