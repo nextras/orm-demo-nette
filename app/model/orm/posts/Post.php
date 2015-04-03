b87786e08e491720e759a12d3c639c58fda22925
hrach <hrach.cz@gmail.com>
2015-04-03 23:33:52 +0200

    Post entity: better phpdoc

--- app/model/orm/posts/Post.php
+++ app/model/orm/posts/Post.php
@@ -3,6 +3,7 @@
 namespace OrmDemo;
 
 use Nette\Utils\DateTime;
+use Nextras\Orm\Collection\ICollection;
 use Nextras\Orm\Entity\Entity;
 use Nextras\Orm\Relationships\OneHasMany;
 
@@ -13,7 +14,7 @@ use Nextras\Orm\Relationships\OneHasMany;
  * @property string $content
  * @property DateTime $createdAt
  * @property OneHasMany|Comment[] $allComments {1:n CommentsRepository}
- * @property-read Comment[] $comments {virtual}
+ * @property-read ICollection|Comment[] $comments {virtual}
  * @property Tag[] $tags {m:n TagsRepository primary}
  */
 class Post extends Entity
